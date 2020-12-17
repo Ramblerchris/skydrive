8fc899a6aa246648e6f2d35f276b0d64be99d769
unknown <wuyishun_kmk@outlook.com>
2020-12-17 23:22:42 +0800

    add SQL

--- doc/分页.sql
+++ doc/分页.sql
@@ -8,3 +8,10 @@ SELECT * from  tbl_file where id>31 limit 1;
 SELECT * from  tbl_file where  id BETWEEN 30 and 40  limit 10;
 
 
+
+select * from skydrive.tbl_user_file  where id < 133  order by id desc limit 4;
+select * from skydrive.tbl_user_file  where id < 129  order by id desc limit 40;
+select max(id) from skydrive.tbl_user_file  ;
+select max(id) from skydrive.tbl_user_file order by id desc ;
+
+
